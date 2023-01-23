333d4e44365b2ce4863ee15e3eec34c580eb63e2
Troy Wilson <102485470+Buggerdo@users.noreply.github.com>
2023-01-23 15:19:08 -0500

    moving to chapter two

--- Code/Chapter_01/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ Code/Chapter_01/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace MyFirstConsoleApp
+﻿using static System.Console;
+
+namespace MyFirstConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            TryAnIf();
+            TrySomeLoops();
+            TryAnIfElse();
+        }
+
+        private static void TryAnIfElse()
+        {
+            int x = 5;
+            int y = 10;
+            if (x == y)
+            {
+                WriteLine($"x is {y}");
+            }
+            else
+            {
+                WriteLine($"x is not {y}");
+            }
+        }
+
+        private static void TrySomeLoops()
+        {
+            int count = 0;
+            while (count < 10)
+            {
+                count++;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                count--;
+            }
+            WriteLine($"The answer is {count}");
+        }
+
+        private static void TryAnIf()
+        {
+            int someValue = 4;
+            string name = "Bob";
+            if (someValue == 3 && name == "Joe")
+            {
+                WriteLine("x is 3 and the name is Joe");
+            }
+            WriteLine("This line runs");
         }
     }
 }
