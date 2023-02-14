d41d5fd2518b0d10cfc4f1ef414b29e24210029c
Troy Wilson <102485470+Buggerdo@users.noreply.github.com>
2023-02-14 13:42:23 -0500

    pick random cards

---
+++ Code/Chapter_03/PickRandomCards/PickRandomCards/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] cards =PickSomeCards(5);
+
+            foreach (string card in cards)
+            {
+                Console.WriteLine(card);
+            }
+        }
+
+        private static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] test = { "hi", "there" };
+            return test;
+        }
+    }
+}
