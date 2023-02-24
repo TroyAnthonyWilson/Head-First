c6851b1eb20ae80982873c5dd773a234f783c626
Troy Wilson <102485470+Buggerdo@users.noreply.github.com>
2023-02-24 12:57:57 -0500

    I need to code more often

---
+++ Code/Chapter_03/Testing/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Clown clown = new Clown("Boffo", 14);
+            clown.TalkAboutYourself();
+            Clown clown1 = new Clown("Biff", 16);
+            clown1.TalkAboutYourself();
+        }
+    }
+
+    public class Clown
+    {
+        private string Name { get; set; }
+        private int Height { get; set; }
+
+        public Clown(string name, int height)
+        {
+            Name = name;
+            Height = height;
+        }
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine($"My name is {Name} nad I'm {Height} inches tall.");
+        }
+    }
+}
