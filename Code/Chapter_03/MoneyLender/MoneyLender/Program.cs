ff81de218861dc2808d20b58e434117e126018f0
Troy Wilson <102485470+Buggerdo@users.noreply.github.com>
2023-02-28 13:04:47 -0500

    Update Program.cs

--- Code/Chapter_03/MoneyLender/MoneyLender/Program.cs
+++ Code/Chapter_03/MoneyLender/MoneyLender/Program.cs
@@ -4,28 +4,7 @@
     {
         static void Main()
         {
-            //list of guys
-            List<Guy> listOfGuys = new List<Guy>();
-            listOfGuys.Add(new Guy("Bob", 100));
-            listOfGuys.Add(new Guy("Joe", 50));
-
-            PrintAllGuysInfo(listOfGuys);
-
-            string giver = "Bob";
-            string receiver = "Joe";
-
-            Bank.TransferCash(listOfGuys[listOfGuys.FindIndex(guy => guy.Name == giver)], listOfGuys[listOfGuys.FindIndex(guy => guy.Name == receiver)], 10);
-
-            PrintAllGuysInfo(listOfGuys);
-        }
-
-        //print all guys info
-        static void PrintAllGuysInfo(List<Guy> listOfGuys)
-        {
-            foreach (Guy guy in listOfGuys)
-            {
-                guy.WriteMyInfo();
-            }
+            Menu.PrintMenu();
         }
     }
 }
