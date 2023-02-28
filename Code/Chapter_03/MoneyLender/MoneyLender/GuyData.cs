1eeb71c8bf47e3e41625e748d1a001ffa53f3f60
Troy Wilson <102485470+Buggerdo@users.noreply.github.com>
2023-02-28 13:04:42 -0500

    Create GuyData.cs

---
+++ Code/Chapter_03/MoneyLender/MoneyLender/GuyData.cs
@@ -0,0 +1,20 @@
+﻿namespace MoneyLender
+{
+    public class GuyData
+    {
+        public List<Guy> listOfGuys = new List<Guy>();
+
+        public GuyData() {}
+
+        //add new guy
+        public void AddGuy()
+        {
+            Console.WriteLine("Enter name of new guy:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter amount of cash:");
+            int amount = int.Parse(Console.ReadLine());
+            listOfGuys.Add(new Guy(name, amount));
+        }
+       
+    }
+}
