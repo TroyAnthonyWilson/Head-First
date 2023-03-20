8864e34a5f06b6eb48836faad10bdf026597de8a
Troy Wilson <102485470+Buggerdo@users.noreply.github.com>
2023-03-20 16:00:32 -0400

    reference

---
+++ Code/Chapter_04/Reference/Reference/Elephant.cs
@@ -0,0 +1,19 @@
+﻿namespace Reference
+{
+    public class Elephant
+    {
+        private string Name { get; set; }
+        private int EarSize { get; set; }
+
+        public Elephant(string name, int earSize)
+        {
+            Name = name;
+            EarSize = earSize;            
+        }
+
+        public void WhoAmI()
+        {
+            Console.WriteLine($"My name is {Name} and my ears are {EarSize} inches tall.");
+        }
+    }
+}
---
+++ Code/Chapter_04/Reference/Reference/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Reference
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Elephant Lucinda = new Elephant("Lucinda", 33);
+            Elephant Lloud = new Elephant("Lloyd", 40);
+
+            do
+            {
+                Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap.");
+                string input = Console.ReadLine();
+                bool isNumber = int.TryParse(input, out int number);
+                if (isNumber)
+                {
+                    switch (number)
+                    {
+                        case 1:
+                            Lloud.WhoAmI();
+                            break;
+                        case 2:
+                            Lucinda.WhoAmI();
+                            break;
+                        case 3:
+                            (Lloud, Lucinda) = (Lucinda, Lloud);
+                            break;
+                        case 4:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            } while (true);
+        }
+    }
+}
